fb3ae51aa37087cb2fa458b33782ff0a90d6ad64
Leonardo Ciocan <leonardo.ciocan@outlook.com>
2015-09-04 23:01:41 +0100

    Added support for adding boxes with a given structure

--- ProjectBoxCore/views.py
+++ ProjectBoxCore/views.py
@@ -1,9 +1,10 @@
 from json import dumps
 import json
 from bson import json_util, ObjectId
-from django.http import HttpResponse
+from django.http import HttpResponse, QueryDict
 from django.shortcuts import render
 from pymongo import MongoClient
+from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 
 client = MongoClient("127.0.0.1", 27017)
@@ -11,17 +12,26 @@ boxes = client.db.boxes
 
 class Box(APIView):
     def put(self, request, format=None):
-        name = request.PUT["name"]
-        boxes.insert({"name": name})
-        return HttpResponse(request.user.username)
+        print("xyu")
+        print(request.body)
+        arr = json.loads(request.body.decode("utf-8"))
+        print(arr)
+        name = arr["name"]
+        print(name)
+        structure = arr["structure"]
+        print(structure)
+        boxes.insert({"name": name , "structure" : structure})
+        return HttpResponse("")
 
     def post(self,request,format=None):
         data = {}
-        for key,value in request.POST.items():
+
+        print(request.POST)
+        for key, value in request.POST.items():
             data[key] = value
         del data["id"]
         if "item_id" not in request.POST:
-            data["_id"] =  ObjectId()
+            data["_id"] = ObjectId()
             print(data)
             boxes.update({"_id": ObjectId(request.POST["id"])},
                 {
@@ -31,17 +41,23 @@ class Box(APIView):
                 })
         else:
             del data["item_id"]
-            for key,value in data.items():
-                boxes.update({"_id": ObjectId(request.POST["id"]) , "_data._id" : ObjectId(request.POST.get("item_id", ""))},
+            for key, value in data.items():
+                boxes.update({"_id": ObjectId(request.POST["id"]),
+                              "_data._id": ObjectId(request.POST.get("item_id", ""))},
                     {
                         "$set": {
                             "_data.$." + key: value
                         }
                     })
-        return HttpResponse("")
+        return HttpResponse([data["_id"] if "_id" in data else ""])
 
     def get(self,request,format=None):
-        return HttpResponse(json.dumps(list(boxes.find()), sort_keys=True, indent=4, default=json_util.default))
+        print([request.GET["id"] if "id" in request.GET else "none"])
+        if "id" in request.GET:
+            return HttpResponse(json.dumps(list(boxes.find({"_id": ObjectId(request.GET["id"])})),
+                                           sort_keys=True, indent=4, default=json_util.default))
+        else:
+            return HttpResponse(json.dumps(list(boxes.find()), sort_keys=True, indent=4, default=json_util.default))
 
 class User(APIView):
     def get(self , request , fromat=None):
